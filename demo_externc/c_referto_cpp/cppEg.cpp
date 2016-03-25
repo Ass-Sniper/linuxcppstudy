e30c99ea7a1eef6f794d8f0cbda0e07a6280becd
Ass-Sniper <zoukaiass@gmail.com>
2016-03-25 21:53:44 +0800

    extern C deep implication

---
+++ demo_externc/c_referto_cpp/cppEg.cpp
@@ -0,0 +1,6 @@
+/*c++ source file: cppEg.cpp*/
+#include "cppEg.h"
+int add(int x, int y)
+{
+	return x + y;
+}
---
+++ demo_externc/c_referto_cpp/cppEg.h
@@ -0,0 +1,5 @@
+/*c++ header file : cppEg.h*/
+#ifndef CPP_EG_H
+#define CPP_EG_H
+extern int add(int x, int y);
+#endif
